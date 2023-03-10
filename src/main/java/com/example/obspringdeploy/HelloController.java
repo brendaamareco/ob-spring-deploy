0e27d99f97f20dc118313cbead5272be75f426c8
“brenda-mareco” <brendaamareco@gmail.com>
2023-03-09 21:21:22 -0300

    downgraded java version

--- src/main/java/com/example/obspringdeploy/HelloController.java
+++ src/main/java/com/example/obspringdeploy/HelloController.java
@@ -21,35 +21,33 @@ public class HelloController
     @GetMapping("/bootstrap")
     public String bootstrap()
     {
-        return """
-                <!doctype html>
-                <html lang="en">
-                  <head>
-                    <!-- Required meta tags -->
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                                
-                    <!-- Bootstrap CSS -->
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                                
-                    <title>Hello, world!</title>
-                  </head>
-                  <body>
-                    <h1>Hola mundo desde Spring Boot</h1>
-                    <a class="btn btn-primary" href="https://www.google.com">Google</a>
-                                
-                    <!-- Optional JavaScript; choose one of the two! -->
-                                
-                    <!-- Option 1: Bootstrap Bundle with Popper -->
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-                                
-                    <!-- Option 2: Separate Popper and Bootstrap JS -->
-                    <!--
-                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-                    -->
-                  </body>
-                </html>
-                """;
+        return "<!doctype html>\n" +
+                "<html lang=\"en\">\n" +
+                "  <head>\n" +
+                "    <!-- Required meta tags -->\n" +
+                "    <meta charset=\"utf-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "\n" +
+                "    <!-- Bootstrap CSS -->\n" +
+                "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">\n" +
+                "\n" +
+                "    <title>Hello, world!</title>\n" +
+                "  </head>\n" +
+                "  <body>\n" +
+                "    <h1>Hola mundo desde Spring Boot</h1>\n" +
+                "    <a class=\"btn btn-primary\" href=\"https://www.google.com\">Google</a>\n" +
+                "\n" +
+                "    <!-- Optional JavaScript; choose one of the two! -->\n" +
+                "\n" +
+                "    <!-- Option 1: Bootstrap Bundle with Popper -->\n" +
+                "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM\" crossorigin=\"anonymous\"></script>\n" +
+                "\n" +
+                "    <!-- Option 2: Separate Popper and Bootstrap JS -->\n" +
+                "    <!--\n" +
+                "    <script src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js\" integrity=\"sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p\" crossorigin=\"anonymous\"></script>\n" +
+                "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js\" integrity=\"sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF\" crossorigin=\"anonymous\"></script>\n" +
+                "    -->\n" +
+                "  </body>\n" +
+                "</html>";
     }
 }
